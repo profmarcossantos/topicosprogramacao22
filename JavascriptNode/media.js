cb5ffa0b0cf3955ab10430ee36b5363d50fdf8d7
Marcos Santos <marcos1003@hotmail.com>
2022-12-02 10:41:39 -0300

    Conteúdo 05

---
+++ JavascriptNode/media.js
@@ -0,0 +1,30 @@
+/*
+function soma(v1, v2) {
+    var resultado = v1 + v2
+    return resultado
+}
+*/
+
+/*
+const soma = (v1,v2) => v1 + v2
+
+
+console.log( soma(5,8) );
+console.log( soma(2,7) );
+console.log( soma(1,2) );
+
+*/
+
+/*
+var valor = 10
+while (valor > 0 ) {
+    console.log(valor);
+    valor = valor -1
+}
+*/
+
+//  inicializador, condição, incrementador
+for (let m = 0; m < 10; m = m + 2) {
+    console.log(m);
+
+}
